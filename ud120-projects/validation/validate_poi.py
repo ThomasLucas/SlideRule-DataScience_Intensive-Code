25acf02e0ec92941fc0d7ef5e34fdc8142642db2
Thomas LUCAS <thomas.lucas88@gmail.com>
2016-02-20 17:12:33 +0000

    Validation mini-project

--- ud120-projects/validation/validate_poi.py
+++ ud120-projects/validation/validate_poi.py
@@ -15,6 +15,10 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn import cross_validation
+
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
 ### first element is our labels, any added elements are predictor
@@ -28,5 +32,23 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+
+# Version 1 - Same dataset for both training and testing
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+prediction = clf.predict(features)
+
+print "Version1 - Accuracy: " + str(accuracy_score(labels, prediction))
+
+# Version 2 - Use cross validation for splitting the dataset into a training set and a test set
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+clf2 = tree.DecisionTreeClassifier()
+clf2 = clf2.fit(features_train, labels_train)
+prediction2 = clf2.predict(features_test)
+
+print "Version2 - Accuracy: " + str(accuracy_score(labels_test, prediction2))
+
+
+
 
 
